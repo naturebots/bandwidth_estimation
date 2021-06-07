7ea1145d5dd9f81ea13ff88062284ea68fcd8505
M <???>
2021-06-07 23:07:42 +0900

    Update: scrape_receiver_report

--- scrapte_receiver_report.sh
+++ scrapte_receiver_report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# usage: ./scrapte_receiver_report.sh log_file
+### usage: ./scrapte_receiver_report.sh log_file ###
 
 array=("Frames decoded" "WebRTC.Video.DroppedFrames.Receiver" "WebRTC.Video.ReceivedPacketsLostInPercent" "WebRTC.Video.DecodedFramesPerSecond" "WebRTC.Video.InterframeDelay95PercentileInMs.S0" "WebRTC.Video.MediaBitrateReceivedInKbps.S0" "WebRTC.Video.MeanFreezeDurationMs" "WebRTC.Video.TimeInHdPercentage" "WebRTC.Video.TimeInBlockyVideoPercentage" "WebRTC.Video.NumberResolutionDownswitchesPerMinute" "WebRTC.Video.NumberFreezesPerMinute" "WebRTC.Video.HarmonicFrameRate" "WebRTC.Video.MediaBitrateReceivedInKbps" "WebRTC.Video.InterframeDelay95PercentileInMs")
 
@@ -8,9 +8,9 @@ array2=("WebRTC.Video.ReceiveStreamLifetimeInSeconds" "WebRTC.Video.MeanTimeBetw
 
 array3=("WebRTC.Call.EstimatedSendBitrateInKbps" "WebRTC.Call.PacerBitrateInKbps" "WebRTC.Call.VideoBitrateReceivedInBps" "WebRTC.Call.AudioBitrateReceivedInBps" "WebRTC.Call.RtcpBitrateReceivedInBps" "WebRTC.Call.BitrateReceivedInBps")
 
-data=()
+values=()
 
-function print_parameter_list(){
+function print_all_parameters(){
   for ((i=0; i<${#array[@]}; i++)); do
     echo -n \"${array[$i]}\",
   done
@@ -22,51 +22,54 @@ function print_parameter_list(){
   for ((i=0; i<${#array3[@]}; i++)); do
     echo -n \"${array3[$i]}\",
   done
+  echo
 }
 
-print_parameter_list
-echo
+function scrapte_values() {
+  for ((i=0; i<${#array[@]}; i++)); do
+    values=("${values[@]}" `cat $1 | grep "${array[$i]} " | sed -e "s/${array[$i]} //g"`)
+  done
 
-for ((i=0; i<${#array[@]}; i++)); do
-  data=("${data[@]}" `cat $1 | grep "${array[$i]} " | sed -e "s/${array[$i]} //g"`)
-done
+  for ((i=0; i<${#array2[@]}; i++)); do
+    values=("${values[@]}" `cat $1 | grep "${array2[$i]}" | sed -e "s/^.*${array2[$i]} //g"`)
+  done
 
-for ((i=0; i<${#array2[@]}; i++)); do
-  data=("${data[@]}" `cat $1 | grep "${array2[$i]}" | sed -e "s/^.*${array2[$i]} //g"`)
-done
-
-for ((i=0; i<${#array3[@]}; i++)); do
-  data=("${data[@]}" "`cat $1 | grep "${array3[$i]}" | sed -e "s/^.*${array3[$i]}, periodic_samples:[0-9]*, //g"`")
-done
+  for ((i=0; i<${#array3[@]}; i++)); do
+    values=("${values[@]}" "`cat $1 | grep "${array3[$i]}" | sed -e "s/^.*${array3[$i]}, periodic_samples:[0-9]*, //g"`")
+  done
+}
 
 function print_result(){
-  for ((i=0; i<${#data[@]}; i++)); do
-    echo -n \"${data[$i]}\",
+  for ((i=0; i<${#values[@]}; i++)); do
+    echo -n \"${values[$i]}\",
   done
   echo
 }
 
+function print_debug_info() {
+ for ((i=0; i<${#array[@]}; i++)); do
+   cat $1 | grep "${array[$i]}"
+ done
+
+ for ((i=0; i<${#array2[@]}; i++)); do
+   cat $1 | grep ${array2[$i]}
+ done
+
+ for ((i=0; i<${#array3[@]}; i++)); do
+   cat $1 | grep ${array3[$i]}
+ done
+
+ echo -n "number of index is " && echo ${#values[@]}
+}
+
+print_all_parameters
+scrapte_values $1
 print_result
 
+# print_debug_info $1
 
 
-### debug ###
-# for ((i=0; i<${#array[@]}; i++)); do
-#   cat $1 | grep "${array[$i]}"
-# done
-#
-# for ((i=0; i<${#array2[@]}; i++)); do
-#   cat $1 | grep ${array2[$i]}
-# done
-#
-# for ((i=0; i<${#array3[@]}; i++)); do
-#   cat $1 | grep ${array3[$i]}
-# done
-#
-# echo ${#data[@]}
-
-
-### list of variables ###
+### list of parameters ###
 # WebRTC.Video.ReceiveStreamLifetimeInSeconds
 # Frames decoded
 # WebRTC.Video.DroppedFrames.Receiver

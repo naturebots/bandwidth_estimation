b8fb663f6c11ac302d0de5b5fd53713b5131ede7
M <???>
2021-06-13 08:14:39 +0900

    Update: scrape_receiver_report.sh

--- scrapte_receiver_report.sh
+++ scrapte_receiver_report.sh
@@ -2,11 +2,13 @@
 
 ### usage: ./scrapte_receiver_report.sh log_file ###
 
-array=("Frames decoded" "WebRTC.Video.DroppedFrames.Receiver" "WebRTC.Video.ReceivedPacketsLostInPercent" "WebRTC.Video.DecodedFramesPerSecond" "WebRTC.Video.InterframeDelay95PercentileInMs.S0" "WebRTC.Video.MediaBitrateReceivedInKbps.S0" "WebRTC.Video.MeanFreezeDurationMs" "WebRTC.Video.TimeInHdPercentage" "WebRTC.Video.TimeInBlockyVideoPercentage" "WebRTC.Video.NumberResolutionDownswitchesPerMinute" "WebRTC.Video.NumberFreezesPerMinute" "WebRTC.Video.HarmonicFrameRate" "WebRTC.Video.MediaBitrateReceivedInKbps" "WebRTC.Video.InterframeDelay95PercentileInMs")
+array=("Frames decoded" "WebRTC.Video.DroppedFrames.Receiver" "WebRTC.Video.ReceivedPacketsLostInPercent" "WebRTC.Video.DecodedFramesPerSecond" "WebRTC.Video.InterframeDelay95PercentileInMs.S0" "WebRTC.Video.MediaBitrateReceivedInKbps.S0" "WebRTC.Video.TimeInHdPercentage" "WebRTC.Video.TimeInBlockyVideoPercentage" "WebRTC.Video.NumberResolutionDownswitchesPerMinute" "WebRTC.Video.NumberFreezesPerMinute" "WebRTC.Video.HarmonicFrameRate" "WebRTC.Video.MediaBitrateReceivedInKbps" "WebRTC.Video.InterframeDelay95PercentileInMs")
 
 array2=("WebRTC.Video.ReceiveStreamLifetimeInSeconds" "WebRTC.Video.MeanTimeBetweenFreezesMs" "WebRTC.Video.DroppedFrames.RenderQueue")
 
 array3=("WebRTC.Call.EstimatedSendBitrateInKbps" "WebRTC.Call.PacerBitrateInKbps" "WebRTC.Call.VideoBitrateReceivedInBps" "WebRTC.Call.AudioBitrateReceivedInBps" "WebRTC.Call.RtcpBitrateReceivedInBps" "WebRTC.Call.BitrateReceivedInBps")
+
+all_arr=("${array[@]}" "${array2[@]}" "${array3[@]}")
 
 values=()
 
@@ -25,6 +27,7 @@ function print_all_parameters(){
   echo
 }
 
+
 function scrapte_values() {
   for ((i=0; i<${#array[@]}; i++)); do
     values=("${values[@]}" `cat $1 | grep "${array[$i]} " | sed -e "s/${array[$i]} //g"`)
@@ -39,6 +42,12 @@ function scrapte_values() {
   done
 }
 
+function print_values(){
+  for ((i=0; i<${#all_arr[@]}; i++)); do
+    echo \"${all_arr[$i]}\", \"${values[$i]}\"
+  done
+}
+
 function print_result(){
   for ((i=0; i<${#values[@]}; i++)); do
     echo -n \"${values[$i]}\",
@@ -48,7 +57,7 @@ function print_result(){
 
 function print_debug_info() {
  for ((i=0; i<${#array[@]}; i++)); do
-   cat $1 | grep "${array[$i]}"
+   cat $1 | grep "${array[$i]} "
  done
 
  for ((i=0; i<${#array2[@]}; i++)); do
@@ -62,11 +71,12 @@ function print_debug_info() {
  echo -n "number of index is " && echo ${#values[@]}
 }
 
-print_all_parameters
+# print_all_parameters
 scrapte_values $1
-print_result
+print_values
+#print_result
 
-# print_debug_info $1
+#print_debug_info $1
 
 
 ### list of parameters ###
